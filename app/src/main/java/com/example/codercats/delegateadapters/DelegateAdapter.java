65d85923f517c117c982dc670d88174f2dab62d9
Alexis Jouhault <alexis.jouhault.ext@francetv.fr>
2017-06-20 10:34:46 -0700

    added recycler view dependency and butterknife

---
+++ app/src/main/java/com/example/codercats/delegateadapters/DelegateAdapter.java
@@ -0,0 +1,12 @@
+package com.example.codercats.delegateadapters;
+
+/**
+ * Created by alexisjouhault on 20/06/2017.
+ *
+ */
+
+public class DelegateAdapter {
+
+
+
+}
--- app/src/main/java/com/example/codercats/delegateadapters/MainActivity.java
+++ app/src/main/java/com/example/codercats/delegateadapters/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.codercats.delegateadapters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.mainactivity_recyclerview)
+    RecyclerView mRecyclerView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
+
+
 }
